78381326037f713196a18340a87e0190c20d5f47
Phil10245 <65542336+Phil10245@users.noreply.github.com>
2020-09-19 03:30:46 +0200

    added a test file
    needs completation
    and replacement by unittest unit?

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
 import random
+from classes import *
 
-lst = [1]*3
-random.shuffle(lst)
-print(lst)
+try:
+    P = Player("Test", 1, 0)
+    print("test 1 ok")
+    print(P)
+except:
+    print("Player instantiaten doesn't work")
+    raise
+try:
+    cards_count = {"green": 0, "blue":0, "red": 0, "blck": 0}
+    P.combine_ressources_with_collected_cards(cards_count)
+    print("no error when calling combine-func")
+except:
+    print("combine is not working")
+    raise
+
+try:
+    ob = OpenBoard()
+    P.pick_crd(ob, 1, RessourceStack(2))
+    print("pick_crd works")
+except:
+    print("pick_crd doesnt work")
+    raise
