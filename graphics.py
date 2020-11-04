a2cbb4a50e96fe8387b9129a31e23ae5259d390c
Phil10245 <65542336+Phil10245@users.noreply.github.com>
2020-11-04 23:06:45 +0100

    ad graphics.py to manage all images.
    ad Graphics/ folder to game dir.

---
+++ graphics.py
@@ -0,0 +1,33 @@
+'''module to load graphics, convert them to pygame-surface and use them in the main.py.'''
+
+import pygame as g
+import os
+
+g.init()
+
+#Set up Display
+DISPLAY = g.display.set_mode((0, 0), g.FULLSCREEN)
+
+#gaming board
+path = "Graphics/Board/"
+name = "background_board.jpg"
+BACKGROUND = g.image.load(os.path.join(path, name))
+BACKGROUND.convert()
+
+#Tokens
+path = "Graphics/Tokens/"
+name = "Redtoken.png"
+REDTOKEN = g.image.load(os.path.join(path, name))
+REDTOKEN.convert()
+name = "Greentoken.png"
+GREENTOKEN = g.image.load(os.path.join(path, name))
+GREENTOKEN.convert()
+name = "Bluetoken.png"
+BLUETOKEN = g.image.load(os.path.join(path, name))
+BLUETOKEN.convert()
+name = "Blacktoken.png"
+BLACKTOKEN = g.image.load(os.path.join(path, name))
+BLACKTOKEN.convert()
+name = "Whitetoken.png"
+WHITETOKEN = g.image.load(os.path.join(path, name))
+WHITETOKEN.convert()
